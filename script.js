c13dc850f3c4f230a72753603463b4b92bc72791
Shozzy <shozzy95@gmail.com>
2024-01-08 15:44:56 +0200

    commit callback script

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 'use strict';
 
-function zcall(arg, callback){
-    console.log(`call ${arg}`);
-    callback();
-}
+// function loadScript(src, callback) {
+//     let script = document.createElement('script');
+//     script.src = src;
+//     script.onload = () => callback(script, 1);
+//     document.head.append(script);
+    
+//   }
 
-function zdone(){
-    console.log(`done`);
-}
+// loadScript('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js', (script, a) => {
+//   alert(`Здорово, скрипт ${script.src} загрузился`);
+//   alert( _ ); // функция, объявленная в загруженном скрипте
+//   alert( a );
+// });
 
-zcall('test', zdone);
+function loadScript(src, callback) {
+    let script = document.createElement('script');
+    script.src = src;
+    script.onerror = function() { alert(`error ${this.src}`) };
+    script.onload = () => callback(script);
+    alert('test1');
+    document.head.append(script); //выполнение всех предыдущих шагов происходит после аппенда на страницу
+  }
+
+loadScript('script1.js', function(script) {
+  // эта функция вызовется после того, как загрузится скрипт
+  alert(script.src);
+  alert(sayHello('TEST')); // теперь всё работает
+});
