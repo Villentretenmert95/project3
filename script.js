e65a8efe994c0cfe4e50d7710c19d504e39130e8
Shozzy <shozzy95@gmail.com>
2024-01-08 19:07:29 +0200

    commit object script

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 'use strict';
 
-// function loadScript(src, callback) {
-//     let script = document.createElement('script');
-//     script.src = src;
-//     script.onload = () => callback(script, 1);
-//     document.head.append(script);
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function(){
+        console.log('Test');
+    }
+};
+
+options.makeTest();
+
+console.log(options.colors.bg);
+
+const {border, bg} = options.colors; //destructurization
+console.log(border);
+
+// delete options.name;
+
+// console.log(options);
+
+for (let key in options) {
+    if (typeof options[key] == 'object'){
+        for (let key1 in options[key]){
+            console.log(`Свойство ${key1} имеет значение ${options[key][key1]}`);
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    }
     
-//   }
+}
 
-// loadScript('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js', (script, a) => {
-//   alert(`Здорово, скрипт ${script.src} загрузился`);
-//   alert( _ ); // функция, объявленная в загруженном скрипте
-//   alert( a );
-// });
-
-function loadScript(src, callback) {
-    let script = document.createElement('script');
-    script.src = src;
-    script.onerror = function() { alert(`error ${this.src}`) };
-    script.onload = () => callback(script);
-    alert('test1');
-    document.head.append(script); //выполнение всех предыдущих шагов происходит после аппенда на страницу
-  }
-//  эта функция вызовется после того, как загрузится скрипт
-loadScript('script1.js', script => { alert(script.src);  alert(sayHello('TEST')); });
+console.log(Object.keys(options).length);
---
+++ script3.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// function loadScript(src, callback) {
+//     let script = document.createElement('script');
+//     script.src = src;
+//     script.onload = () => callback(script, 1);
+//     document.head.append(script);
+    
+//   }
+
+// loadScript('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js', (script, a) => {
+//   alert(`Здорово, скрипт ${script.src} загрузился`);
+//   alert( _ ); // функция, объявленная в загруженном скрипте
+//   alert( a );
+// });
+
+function loadScript(src, callback) {
+    let script = document.createElement('script');
+    script.src = src;
+    script.onerror = function() { alert(`error ${this.src}`) };
+    script.onload = () => callback(script);
+    alert('test1');
+    document.head.append(script); //выполнение всех предыдущих шагов происходит после аппенда на страницу
+  }
+//  эта функция вызовется после того, как загрузится скрипт
+loadScript('script1.js', script => { alert(script.src);  alert(sayHello('TEST')); });
